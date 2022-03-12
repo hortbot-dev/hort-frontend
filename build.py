c177bcb49ef5cb2a8ef75d2c9cd487076dc8883b
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-12 15:56:09 +0900

    Update build.py

--- build.py
+++ build.py
@@ -2,14 +2,13 @@ from jinja2 import Template, Environment, FileSystemLoader
 from os import path as opath
 import os
 
-def change(path):
+def change(env, path):
     for p in os.listdir(path):
         if opath.isdir(p):
-            change(path + p)
+            change(Environment(loader=FileSystemLoader(p, encoding='utf8')), path + p)
         else:
-            env = Environment(loader=FileSystemLoader(p, encoding='utf8'))
             tpl = env.get_template(p)
             with open("./html/" + tpl.replace(".j2", ".html"), "w") as f:
                 f.write(tpl.render())
                 
-change("jinja2")
+change(Environment(loader=FileSystemLoader("jinja2", encoding='utf8')), "jinja2")
